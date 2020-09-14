de89b798168a6d447e2c54f0c426869913e479ff
Sonalicodeastu <sonali.nimgade@codeastu.com>
2020-09-14 21:09:44 +0530

    validations

--- src/App.js
+++ src/App.js
@@ -4,69 +4,70 @@ import subheading from'./left.svg';
 import './App.css';
 
 
-function validate(address, apt, zipcode, city, state) {
-  
-  return {
-    address: address.length === 0, 
-    apt: apt.length === 0,
-    zipcode: zipcode.length === 0,
-    city: city.length === 0, 
-    state: state.length === 0  
-    
-  };
-}
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      address: '',
-      apt: '',
-      zipcode:'',
-      city:'',
-      state:'',
-      touched: {
-        address: false,
-        apt: false,
-        zipcode:false,
-        city:false,
-        state:false,
-      },
-    };
-  }
-  handleaddressChange = (evt) => {
-    this.setState({ address: evt.target.value });
-  }
-  
-  handleaptChange = (evt) => {
-    this.setState({ apt: evt.target.value });
-  }
-  
-  handlezipcodeChange = (evt) => {
-    this.setState({ zipcode: evt.target.value });
-  }
-  
-  handlecityChange = (evt) => {
-    this.setState({ city: evt.target.value });
-  }
-  handlestateChange = (evt) => {
-    this.setState({ state: evt.target.value });
-  }
-  handleBlur = (field) => (evt) => {
-    this.setState({
-      touched: { ...this.state.touched, [field]: true },
-    });
-  }
 
+  
+class App extends Component {
+  state = {
+    inputs: {
+      address: "",
+      apt: "",
+      zipcode: "",
+      city:"",
+      state:""
+    },
+    errors: {
+      address: false,
+      apt: false,
+      zipcode: false,
+      city:false,
+      state:false,
+    },
+    disable:true
+
+  };
+  handleOnChange = event => {
+    const { name, value } = event.target;
+
+      this.setState({
+        inputs: {
+          ...this.state.inputs,
+          [name]: value
+        },
+        errors: {
+          ...this.state.errors,
+          [name]: false
+        }
+      });
+  };
+
+
+
+  handleOnBlur = event => {
+    const { inputs } = this.state;
+    if (inputs[event.target.name].length === 0) {
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          [event.target.name]: true
+        }
+      });
+    }
+  };
+  
+  enabled = () => {
+    const { address, apt, zipcode, city, state } = this.state.errors;
+    console.log(address);
+    console.log(apt);
+    console.log(zipcode);
+    console.log(city);
+    console.log(state);
+    console.log(this.state.errors && address && apt && zipcode && city && state);
+    return (this.state.errors && address && apt && zipcode && city && state)
+};
+//&& !address && !apt && !zipcode && !city && !state
 
   render(){
-    //const { errors } = this.state;
-    const errors = validate(this.state.address, this.state.apt,this.state.zipcode,this.state.city,this.state.state);
-    const shouldMarkError = (field) => {
-      const hasError = errors[field];
-      const shouldShow = this.state.touched[field];
-      
-      return hasError ? shouldShow : false;
-    };
+    
   return (
     <div className="heading">
       <header className="center">
@@ -74,29 +75,66 @@ class App extends Component {
         <img src={heading} className="heading" alt="heading" />
         <img src={subheading} className="svgimage" alt="subheading"/>
     <span className="label">Address</span><br/>
-    <input className="inputField" type="text" onChange={this.handleaddressChange} onBlur={this.handleBlur('address')} value={this.state.address} required />
-    <span className={shouldMarkError('address') ? "error" : "hidden"}>This value is required error</span><br/>
+    <input  type="text"
+    className={
+    this.state.errors.address ? "inputField form-input-fail" : "inputField"
+      }
+      name="address"
+     value={this.state.inputs.address}
+     onChange={this.handleOnChange}
+     onBlur={this.handleOnBlur} 
+     required />
+     <p className="red">{this.state.errors.address ? "This field cannot be Empty" : ""}</p>
     <span className="label">Address (Apt, suite, floor)</span><br/>
-    <input className="inputField"  type="text" onChange={this.handleaptChange} onBlur={this.handleBlur('apt')} value={this.state.apt} required/>
-    <span className={shouldMarkError('apt') ? "error" : "hidden"}>This value is required error</span><br/>
+    <input className={
+    this.state.errors.address ? "inputField form-input-fail" : "inputField"
+      }  type="text" 
+      name="apt"
+      value={this.state.inputs.apt}
+      onChange={this.handleOnChange}
+      onBlur={this.handleOnBlur} 
+        required/>
+        <p className="red">{this.state.errors.apt ? "This field cannot be Empty" : ""}</p>
     <div className="halfGrid"> <span className="label">Zipcode</span><br/>
-      <input className="inputFieldHalf" type="text" onChange={this.handlezipcodeChange} onBlur={this.handleBlur('zipcode')} value={this.state.zipcode}/>
-      <span className={shouldMarkError('zipcode') ? "error" : "hidden"}>This value is required error</span><br/>
-    </div>
+      <input className={
+    this.state.errors.zipcode ? "inputField form-input-fail" : "inputField"
+      }  type="Text"  
+      name="zipcode"
+      value={this.state.inputs.zipcode}
+      onChange={this.handleOnChange}
+      onBlur={this.handleOnBlur} 
+        />
+        <p className="red">{this.state.errors.zipcode ? "This field cannot be Empty" : ""}</p>
+       </div>
+
     <div className="whole">
       <div className="half-left"> <span className="label">City</span><br/>
-        <input className="inputFieldHalf" type="text" onChange={this.handlecityChange} onBlur={this.handleBlur('city')} value={this.state.city}/>
-        <br/><span className={shouldMarkError('city') ? "error" : "hidden"}>This value is required error</span><br/>
+        <input className={
+    this.state.errors.city ? "inputField form-input-fail" : "inputField"
+      }  type="Text"  
+      name="city"
+      value={this.state.inputs.city}
+      onChange={this.handleOnChange}
+      onBlur={this.handleOnBlur} />
+        <br/>
+        <p className="red">{this.state.errors.city ? "This field cannot be Empty" : ""}</p>
       </div>
       <div className="half-right"><span className="label">State</span><br/>
-        <input className="inputFieldHalf" type="text" onChange={this.handlestateChange} onBlur={this.handleBlur('state')} value={this.state.state}/>
-        <br/><span className={shouldMarkError('state') ? "error" : "hidden"}>This value is required error</span><br/>
+        <input className={
+    this.state.errors.state ? "inputField form-input-fail" : "inputField"
+      }  type="Text"  
+      name="state"
+      value={this.state.inputs.state}
+      onChange={this.handleOnChange}
+      onBlur={this.handleOnBlur}/>
+        <br/>
+        <p className="red">{this.state.errors.state ? "This field cannot be Empty" : ""}</p>
       </div>
     </div>
     <div className="check">
-      <input className="checkbox" type="checkbox"/><span className="label">Save my address for future use</span>
+      <input className="checkbox" type="checkbox" checked/><span className="label">Save my address for future use</span>
     </div>
-    <button type="submit" className="borderbutton">Send kit</button>
+    <button type="submit" className="borderbutton" disabled={this.enabled()}>Send kit</button>
     </form>
       </header>
     </div>
