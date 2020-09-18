3bbefabd8013495fc39344d3414822ccd5228aa5
Sonalicodeastu <sonali.nimgade@codeastu.com>
2020-09-18 22:03:40 +0530

    class to functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import heading from "./left.png";
 import subheading from "./left.svg";
 import "./App.css";
 import _ from "underscore";
-class App extends Component {
-  state = {
+function App() {
+  const [initial, later] = useState({
     inputs: {
       address: "",
       apt: "",
@@ -20,184 +20,178 @@ class App extends Component {
       state: false,
     },
     checked: false,
-  };
-  handleOnChange = (event) => {
+  });
+  const handleOnChange = (event) => {
     const { name, value } = event.target;
 
-    this.setState({
+    later({
       inputs: {
-        ...this.state.inputs,
+        ...initial.inputs,
         [name]: value,
       },
       errors: {
-        ...this.state.errors,
+        ...initial.errors,
         [name]: false,
       },
     });
   };
-
-  handleOnBlur = (event) => {
-    const { inputs } = this.state;
+  const handleOnBlur = (event) => {
+    const { inputs } = initial;
+    // console.log(initial);
     if (_.isEmpty(inputs[event.target.name])) {
-      this.setState({
+      later({
         errors: {
-          ...this.state.errors,
+          ...initial.errors,
           [event.target.name]: true,
         },
       });
     }
   };
 
-  enabled = () => {
-    const { address, apt, city, zipcode, state } = this.state.inputs;
+  const enabled = () => {
+    const { address, apt, city, zipcode, state } = initial;
     return (
       _.isEmpty(address) ||
       _.isEmpty(apt) ||
       _.isEmpty(city) ||
       _.isEmpty(zipcode) ||
       _.isEmpty(state) ||
-      !this.state.checked
+      !initial.checked
     );
   };
 
-  handleChange = () => {
-    this.setState({
-      checked: !this.state.checked,
+  const handleChange = () => {
+    later({
+      checked: !initial.checked,
     });
   };
 
-  render() {
-    return (
-      <div className="heading">
-        <header className="center">
-          <form noValidate>
-            <img src={heading} className="heading" alt="heading" />
-            <img src={subheading} className="svgimage" alt="subheading" />
-            <div className="container">
-              <span className="label">Address</span>
-              <br />
-              <input
-                type="text"
-                className={
-                  this.state.errors.address
-                    ? "inputField form-input-fail"
-                    : "inputField"
-                }
-                name="address"
-                value={this.state.inputs.address}
-                onChange={this.handleOnChange}
-                onBlur={this.handleOnBlur}
-                required
-              />
-              <p className="red">
-                {this.state.errors.address ? "This field cannot be Empty" : ""}
-              </p>
-            </div>
-            <div className="container">
-              <span className="label">Address (Apt, suite, floor)</span>
-              <br />
-              <input
-                className={
-                  this.state.errors.apt
-                    ? "inputField form-input-fail"
-                    : "inputField"
-                }
-                type="text"
-                name="apt"
-                value={this.state.inputs.apt}
-                onChange={this.handleOnChange}
-                onBlur={this.handleOnBlur}
-                required
-              />
-              <p className="red">
-                {this.state.errors.apt ? "This field cannot be Empty" : ""}
-              </p>
-            </div>
+  return (
+    <div className="heading">
+      <header className="center">
+        <form noValidate>
+          <img src={heading} className="heading" alt="heading" />
+          <img src={subheading} className="svgimage" alt="subheading" />
+          <div className="container">
+            <span className="label">Address</span>
+            <br />
+            <input
+              type="text"
+              className={
+                initial?.errors?.address
+                  ? "inputField form-input-fail"
+                  : "inputField"
+              }
+              name="address"
+              value={initial?.inputs?.address}
+              onChange={handleOnChange}
+              onBlur={handleOnBlur}
+              required
+            />
+            <p className="red">
+              {initial?.errors?.address ? "This field cannot be Empty" : ""}
+            </p>
+          </div>
+          <div className="container">
+            <span className="label">Address (Apt, suite, floor)</span>
+            <br />
+            <input
+              className={
+                initial?.errors?.apt
+                  ? "inputField form-input-fail"
+                  : "inputField"
+              }
+              type="text"
+              name="apt"
+              value={initial?.inputs?.apt}
+              onChange={handleOnChange}
+              onBlur={handleOnBlur}
+              required
+            />
+            <p className="red">
+              {initial?.errors?.apt ? "This field cannot be Empty" : ""}
+            </p>
+          </div>
 
-            <div className="halfGrid">
+          <div className="halfGrid">
+            {" "}
+            <span className="label">Zipcode</span>
+            <br />
+            <input
+              className={
+                initial?.errors?.zipcode
+                  ? "inputField form-input-fail"
+                  : "inputField"
+              }
+              type="Text"
+              name="zipcode"
+              value={initial?.inputs?.zipcode}
+              onChange={handleOnChange}
+              onBlur={handleOnBlur}
+            />
+            <p className="red">
+              {initial?.errors?.zipcode ? "This field cannot be Empty" : ""}
+            </p>
+          </div>
+
+          <div className="whole">
+            <div className="half-left">
               {" "}
-              <span className="label">Zipcode</span>
+              <span className="label">City</span>
               <br />
               <input
                 className={
-                  this.state.errors.zipcode
+                  initial?.errors?.city
                     ? "inputField form-input-fail"
                     : "inputField"
                 }
                 type="Text"
-                name="zipcode"
-                value={this.state.inputs.zipcode}
-                onChange={this.handleOnChange}
-                onBlur={this.handleOnBlur}
+                name="city"
+                value={initial?.inputs?.city}
+                onChange={handleOnChange}
+                onBlur={handleOnBlur}
               />
-              <p className="red">
-                {this.state.errors.zipcode ? "This field cannot be Empty" : ""}
+              <br />
+              <p className="red-city">
+                {initial?.errors?.city ? "This field cannot be Empty" : ""}
               </p>
             </div>
-
-            <div className="whole">
-              <div className="half-left">
-                {" "}
-                <span className="label">City</span>
-                <br />
-                <input
-                  className={
-                    this.state.errors.city
-                      ? "inputField form-input-fail"
-                      : "inputField"
-                  }
-                  type="Text"
-                  name="city"
-                  value={this.state.inputs.city}
-                  onChange={this.handleOnChange}
-                  onBlur={this.handleOnBlur}
-                />
-                <br />
-                <p className="red-city">
-                  {this.state.errors.city ? "This field cannot be Empty" : ""}
-                </p>
-              </div>
-              <div className="half-right">
-                <span className="label">State</span>
-                <br />
-                <input
-                  className={
-                    this.state.errors.state
-                      ? "inputField form-input-fail"
-                      : "inputField"
-                  }
-                  type="Text"
-                  name="state"
-                  value={this.state.inputs.state}
-                  onChange={this.handleOnChange}
-                  onBlur={this.handleOnBlur}
-                />
-                <br />
-                <p className="red-city">
-                  {this.state.errors.state ? "This field cannot be Empty" : ""}
-                </p>
-              </div>
-            </div>
-            <div className="check">
+            <div className="half-right">
+              <span className="label">State</span>
+              <br />
               <input
-                className="checkbox"
-                type="checkbox"
-                onChange={this.handleChange}
+                className={
+                  initial?.errors?.state
+                    ? "inputField form-input-fail"
+                    : "inputField"
+                }
+                type="Text"
+                name="state"
+                value={initial?.inputs?.state}
+                onChange={handleOnChange}
+                onBlur={handleOnBlur}
               />
-              <span className="label">Save my address for future use</span>
+              <br />
+              <p className="red-city">
+                {initial?.errors?.state ? "This field cannot be Empty" : ""}
+              </p>
             </div>
-            <button
-              type="submit"
-              className="borderbutton"
-              disabled={this.enabled()}
-            >
-              Send kit
-            </button>
-          </form>
-        </header>
-      </div>
-    );
-  }
+          </div>
+          <div className="check">
+            <input
+              className="checkbox"
+              type="checkbox"
+              onChange={handleChange}
+            />
+            <span className="label">Save my address for future use</span>
+          </div>
+          <button type="submit" className="borderbutton" disabled={enabled()}>
+            Send kit
+          </button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
